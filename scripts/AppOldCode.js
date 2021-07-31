70cfe3b7415202838b1ad337e9671b9e83611d33
Alessandro Lisi <alessandro.lisi@mail.polimi.it>
2021-07-31 11:23:06 +0200

    clean up of App.js from TWGL and old obj loader

--- scripts/AppOldCode.js
+++ scripts/AppOldCode.js
@@ -1,11 +1,15 @@
+void
 
-function main() {
+async function main() {
     // Get A WebGL context
     const canvas = document.getElementById('game-canvas');
     const gl = canvas.getContext("webgl2");
     if (!gl) {
         return;
     }
+
+    // Tell the twgl to match position with a_position etc..
+    twgl.setAttributePrefix("a_");
 
     const vs = `#version 300 es
   in vec4 a_position;
@@ -45,12 +49,13 @@ function main() {
   }
   `;
 
+
     // compiles and links the shaders, looks up attribute and uniform locations
-   // const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
+    const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-   // const obj = await loader.load_obj('../assets/models/Queen.obj');
+    const obj = await loader.load_obj('../assets/models/Queen.obj');
 
-   /* const parts = obj.geometries.map(({data}) => {
+    const parts = obj.geometries.map(({data}) => {
         // Because data is just named arrays like this
         //
         // {
@@ -85,12 +90,46 @@ function main() {
             bufferInfo,
             vao,
         };
-    }); */
+    });
 
+    function getExtents(positions) {
+        const min = positions.slice(0, 3);
+        const max = positions.slice(0, 3);
+        for (let i = 3; i < positions.length; i += 3) {
+            for (let j = 0; j < 3; ++j) {
+                const v = positions[i + j];
+                min[j] = Math.min(v, min[j]);
+                max[j] = Math.max(v, max[j]);
+            }
+        }
+        return {min, max};
+    }
+
+    function getGeometriesExtents(geometries) {
+        return geometries.reduce(({min, max}, {data}) => {
+            const minMax = getExtents(data.position);
+            return {
+                min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+                max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+            };
+        }, {
+            min: Array(3).fill(Number.POSITIVE_INFINITY),
+            max: Array(3).fill(Number.NEGATIVE_INFINITY),
+        });
+    }
+
+    const extents = getGeometriesExtents(obj.geometries);
+    const range = m4.subtractVectors(extents.max, extents.min);
+    // amount to move the object so its center is at the origin
+    const objOffset = m4.scaleVector(
+        m4.addVectors(
+            extents.min,
+            m4.scaleVector(range, 0.5)),
+        -1);
     const cameraTarget = [0, 0, 0];
     // figure out how far away to move the camera so we can likely
-
-    const radius = 3
+    // see the object.
+    const radius = m4.length(range) * 1.2;
     let cameraPosition = m4.addVectors(cameraTarget, [
         0,
         2,
@@ -101,6 +140,9 @@ function main() {
     const zNear = radius / 100;
     const zFar = radius * 3;
 
+    function degToRad(deg) {
+        return deg * Math.PI / 180;
+    }
 
     function render(time) {
         time *= 0.001;  // convert to seconds
@@ -112,7 +154,7 @@ function main() {
         let cam_z_pos = document.getElementById("czpos").value/INPUT_SCALE;
 
 
-        /*twgl.resizeCanvasToDisplaySize(gl.canvas);
+        twgl.resizeCanvasToDisplaySize(gl.canvas);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         gl.enable(gl.DEPTH_TEST);
 
@@ -138,8 +180,28 @@ function main() {
             u_view: view,
             u_projection: projection,
         };
- */
 
+        gl.useProgram(meshProgramInfo.program);
+
+        // calls gl.uniform
+        twgl.setUniforms(meshProgramInfo, sharedUniforms);
+
+        // compute the world matrix once since all parts
+        // are at the same space.
+        let u_world = m4.yRotation(time);
+        u_world = m4.translate(u_world, ...objOffset);
+
+        for (const {bufferInfo, vao, material} of parts) {
+            // set the attributes for this part.
+            gl.bindVertexArray(vao);
+            // calls gl.uniform
+            twgl.setUniforms(meshProgramInfo, {
+                u_world,
+                u_diffuse: material.u_diffuse,
+            });
+            // calls gl.drawArrays or gl.drawElements
+            twgl.drawBufferInfo(gl, bufferInfo);
+        }
 
         requestAnimationFrame(render);
     }
