2c086440796f5993fdaf1c5c4b938248547d8429
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2021-08-16 11:54:43 +0200

    feat: better looking disableScroll

--- scripts/Camera.js
+++ scripts/Camera.js
@@ -12,12 +12,22 @@ const CAMERA_SPHERE_RADIUS = 14;
 var keysPressed = {};
 
 
-function disableScroll() {
-    document.body.classList.add("stop-scrolling");
+function enableScroll() {
+    window.onscroll = function() {};
 }
 
-function enableScroll() {
-    document.body.classList.remove("stop-scrolling");
+function disableScroll() {
+    // Get the current page scroll position
+    let scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+    let scrollLeft =
+            window.pageXOffset || document.documentElement.scrollLeft;
+
+        // if any scroll is attempted,
+        // set this to the previous value
+        window.onscroll = function () {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
 }
 
 document.addEventListener('keydown', (e) => {
